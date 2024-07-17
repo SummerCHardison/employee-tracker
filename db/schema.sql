d1380c696cc85767063f3ec730d92678b4da150b
Summer H <sh.hardison.sh@icloud.com>
2024-07-17 19:40:23 -0400

    Adds files and npm dependancies as well as sets up database and tables

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
+
+\c business_db;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+    );
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
